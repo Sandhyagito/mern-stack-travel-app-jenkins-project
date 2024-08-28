5c19cdaaa8e3c4a011002827429787b39b60a01a
Sandhya babu <sandhyababu_18@outlook.com>
2024-08-28 23:37:03 +0530

    updated server.js backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,16 +3,18 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect('mongodb://localhost:27017/wanderland', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// Connect to MongoDB
+mongoose.connect('mongodb://mongodb:27017/wanderland', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB:', err));
 
+// Define a simple route
 app.get('/', (req, res) => {
-  res.send('Hello, Wanderland Travelers!');
+    res.send('welcome to MERN stack project!');
 });
 
+// Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
 
